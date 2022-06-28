2073ff0e7248558f8e06ca470150385b0ca312fa
Beth <bethmelmtv@gmail.com>
2022-06-28 10:06:37 -0700

    worked on first test

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ class LinkedList {
     this.head = new Node(val);
   }
 
-  add(){
-
+  add(val) {
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = new Node(val);
   }
-  removeTail(){}
+
+  removeTail() {}
 
   getList() {
     const listArr = [];
